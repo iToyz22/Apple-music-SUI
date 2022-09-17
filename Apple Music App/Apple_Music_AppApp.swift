a94ddf3b6ee7844fdc9e139002c79f28fb7c55ed
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-09-17 17:25:48 +0300

    Initial Commit

---
+++ Apple Music App/Apple_Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_AppApp.swift
+//  Apple Music App
+//
+//  Created by Anatoliy on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
